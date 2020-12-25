6ebb0fbfabfd0f6aaa2ce2b94aec4f226366dac2
Programmer <programmer.unknowntv@gmail.com>
2020-12-26 03:19:22 +0700

    hapus asas

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log("asass");
 var xx = 123;
 
-
-switch(xx){
-    case 213:
-        break;
-    default:
-        console.log("as")
+switch (xx) {
+  case 213:
+    break;
+  default:
+    console.log("as");
 }
